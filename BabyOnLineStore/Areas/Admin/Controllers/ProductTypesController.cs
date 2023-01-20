6e07a80dc7ea95b6f5e1e14ed3512bf21e0a1bb8
Dyala <dialakul36@gmail.com>
2023-01-20 17:38:18 +0100

    Add project files.

---
+++ BabyOnLineStore/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,29 @@
+﻿using BabyOnLineStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BabyOnLineStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        // passing data till database throw a cunstractor
+        private ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View(_db.ProductTypes.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ BabyOnLineStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BabyOnLineStore.Models;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Http.Headers;
+
+namespace BabyOnLineStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+
+}
