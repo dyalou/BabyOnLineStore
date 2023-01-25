02e69a0f7a9f1779b673d75a376e43e4bb979955
Dyala <dialakul36@gmail.com>
2023-01-25 18:33:50 +0100

    Rename i index page

--- BabyOnLineStore/Models/Items.cs
+++ BabyOnLineStore/Models/Items.cs
@@ -7,17 +7,21 @@ namespace BabyOnLineStore.Models
     {
 
         public int Id { get; set; }
-        [Required]
+
         public string? ItemName { get; set; }
 
         public string? Categories { get; set; }
-        [Required]
+
         public double Price { get; set; }
+
         public string? Type { get; set; }
+
         public string? ProductDescription { get; set; }
+
         public string? Image { get; set; }
+
         public DateTime DataCreated { get; set; }
         [ForeignKey("ProductTypedId")]
-        public ItemsList ProductTypes { get; set; }
+        public ItemsList ItemsList { get; set; }
     }
 }
--- BabyOnLineStore/Models/ItemsList.cs
+++ BabyOnLineStore/Models/ItemsList.cs
@@ -5,8 +5,8 @@ namespace BabyOnLineStore.Models
     public class ItemsList
     {
         public int Id { get; set; }
-        [Required]
-        [Display(Name = "Product Type")]
+        //[Required]
+        //[Display(Name = "Product Type")]
         public string ProductType { get; set; }
 
     }
